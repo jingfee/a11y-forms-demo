e1406ab5f33d95215f05f1d251c036e4ee61b0a5
Sebastian Hew <sebastian.hew@consid.se>
2024-02-18 14:20:49 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'a11y-forms-demo-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'a11y-forms-demo';
+}
